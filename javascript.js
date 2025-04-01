f55810f4163b98b69e9ac2ec26bc1eab820384e1
Cristian Rinaldi <cristianrinaldi0@gmail.com>
2025-04-01 03:44:35 +0200

    add

--- javascript.js
+++ javascript.js
@@ -2,7 +2,13 @@ const imgScorrevoli = document.getElementById("img-scorrevoli");
 let arrayImgScorrevoli = [
   "assets/pompiere.jpg",
   "assets/difesa-personale.jpg",
-  "assets/esculapio.jpg",
+  "assets/caduceo.jpg",
+  "assets/incendi boschivi.jpg",
+  "assets/wrestling.jpg",
+  "assets/blsd for life.png",
+  "assets/pompiere scale.jpg",
+  "assets/blsd-pediatrico.jpeg",
+  "assets/bambini wrestling.jpg",
   "assets/elisoccorso.jpeg",
   "assets/fuoco2.jpg",
 ];
